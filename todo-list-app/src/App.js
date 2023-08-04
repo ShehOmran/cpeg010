c0e93ec86ed80ecc526e4689af6363e4a17d5634
ShehOmran <140434136+ShehOmran@users.noreply.github.com>
2023-08-04 01:07:43 -0600

    Add files via upload

---
+++ todo-list-app/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
+      <div>
+        <Header />
+      </div>
+      <br></br>
+      <TodoList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-list-app/src/components/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TodoList = () => {
+    return (
+      <ul>
+        <li>Learn React</li>
+        <li>Complete Lab 1</li>
+        <li>Complete Lab 2</li>
+        <li>Complete Lab 3</li>
+        <li>Complete Lab 4</li>
+        <li>Complete Lab 5</li>
+        <li>Complete Lab 6</li>
+        <li>Complete Project</li>
+        {/* Add more tasks as needed */}
+      </ul>
+    );
+  };
+
+export default TodoList;
